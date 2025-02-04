a1e124749c78da140d10a083b71c649011932209
NgocTu_ANSV <vungoctu>
2025-02-04 23:00:50 +0700

    Update security step 1

--- tickets/src/main/java/project/tickets_vehicle/tickets/repository/User_Repository.java
+++ tickets/src/main/java/project/tickets_vehicle/tickets/repository/User_Repository.java
@@ -2,8 +2,14 @@ package project.tickets_vehicle.tickets.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import project.tickets_vehicle.tickets.model.user.User;
-
+@Repository
 public interface User_Repository extends JpaRepository<User, Long>{
-
+    @Query(value = "SELECT u FROM User u WHERE u.id = :id")
+    String getRoleByID(@Param("id") long id);
+    @Query(value = "SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
 }
